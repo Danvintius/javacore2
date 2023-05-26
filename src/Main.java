9c49d44362d71180c41300f7200f7ee977b8632a
Danvintius <danila.94@mail.ru>
2023-05-26 15:23:35 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>(Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4));
+    //    intList.filter();
+        System.out.println(intList);
+    }
+}
