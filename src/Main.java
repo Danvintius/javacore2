8ccfb5ffc87a12c6cb330e4cc3f4a05c42386c2e
Danvintius <danila.94@mail.ru>
2023-06-01 23:51:39 +0300

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class Main {
                 filteredList.add(entry);
             }
         }
+            Collections.sort(filteredList);
             System.out.println(filteredList);
     }
 }
